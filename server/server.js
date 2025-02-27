34e9eaa461b6325211093368d31edb92801100a3
Neil <neilseneasow@gmail.com>
2025-02-27 13:01:25 +0530

    Add backend files

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const corsOptions = {
-    origin: "http://localhost:5173" // Removed the trailing slash
+    origin: ["http://localhost:5173", "https://second-chance-ff8xmyxfu-neilseneasows-projects.vercel.app"] // Added the new origin
 }
 
 app.use(cors(corsOptions));
